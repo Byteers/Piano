aa0002620d480c5b5ffc3f91e0e976ebefd78729
Byteers <131696489+Byteers@users.noreply.github.com>
2024-06-06 09:26:10 +0530

    Update Script

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
-//select element
+// Select elements
 const pianoKeysEl = document.querySelectorAll(".piano-keys .key");
 const volumeSliderEl = document.querySelector(".volume-slider input");
 const keyTogglerEl = document.querySelector(".keys-checkbox input");
 
-//audio file location
-let audio = new Audio("/tunes/a.wav");
+// Initialize audio element
+const audio = new Audio();
+audio.src = "tunes/a.wav";
 
-let allKeys = [];
+// Extract keys from the DOM
+const allKeys = Array.from(pianoKeysEl).map(key => key.dataset.key);
 
-//function to play tune
+// Function to play tune
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;
     audio.play();
 
-    //adding class the active key
+    // Adding class to the active key
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
     clickedKey.classList.add("active");
-    //timeout to remove active class
+    // Timeout to remove active class
     setTimeout(() => {
         clickedKey.classList.remove("active");
-    }, 100)
+    }, 100);
 };
 
-//event to get the key pressed by mouse
+// Event to get the key pressed by mouse
 pianoKeysEl.forEach(key => {
-    //adding data key value to all keys array to avoid error
-    allKeys.push(key.dataset.key)
-
-    //calling playtune function when keys clicked
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
-//function to handle volume
+// Function to handle volume
 const handleVolume = (e) => {
     audio.volume = e.target.value;
 };
 
-//function to handle toggle
-const handleToggle = (e) => {
-    //to toggle hide class in each key
+// Function to handle toggle
+const handleToggle = () => {
     pianoKeysEl.forEach(key => key.classList.toggle("hide"));
 };
 
-//function to handle key press
+// Function to handle key press
 const pressedKey = (e) => {
-    allKeys.includes(e.key) && playTune(e.key);
+    if (allKeys.includes(e.key)) {
+        playTune(e.key);
+    }
 };
 
-//event to handle volume slider
+// Event listeners
 keyTogglerEl.addEventListener("click", handleToggle);
-
-//event to handle volume slider
 volumeSliderEl.addEventListener("input", handleVolume);
-
-//event to handle key press by keyboard
 document.addEventListener("keydown", pressedKey);
